2171debc027afa6e13ea5854527a2a83a2b2b62a
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-07 00:56:10 +0100

    initial commit

--- handlers/cart.go
+++ handlers/cart.go
@@ -52,3 +52,54 @@ func GetCart(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(cart)
 }
+
+// UpdateCart updates a cart by ID
+func UpdateCart(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid cart ID", http.StatusBadRequest)
+		return
+	}
+
+	var cart models.Cart
+	if err := db.First(&cart, id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			http.Error(w, "Cart not found", http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	//converts the json request body to Go struct
+	if err := json.NewDecoder(r.Body).Decode(&cart); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	//saves the new cart
+	if err := db.Save(&cart).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//responds with a json format
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(cart)
+}
+
+// DeleteCart deletes a cart by ID
+func DeleteCart(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid cart ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := db.Delete(&models.Cart{}, id).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
