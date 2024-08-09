c054d4ba73a1abbfb7898fbfcfe3f1b90785e46d
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-09 13:09:54 +0100

    feat:added goroutine to handle context since method Send() does not support context

--- utils/mailgun.go
+++ utils/mailgun.go
@@ -23,12 +23,33 @@ func SendOrderConfirmationEmail(toEmail string, orderDetails string) error {
 	subject := "Order Confirmation"
 	body := fmt.Sprintf("Thank you for your order!\n\nOrder Details:\n%s", orderDetails)
 
-	message := mg.NewMessage(sender, subject, body, toEmail) /
+	message := mg.NewMessage(sender, subject, body, toEmail)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	
+
 	defer cancel()
 
-	_, _, err := mg.Send(ctx, message)
-	return err
+	message.SetDeliveryTime(time.Now().Add(time.Second * 10))
+	message.SetReplyTo("no-reply@ecommerce")
+
+	// A goroutine is used to send the email asynchronously since the method Send() does not support context.
+	// Context is used to cancel the email sending operation if it takes too long to prevent the program from hanging.
+
+	resultChan := make(chan error, 1) // Buffered channel of type error to receive the result of the email sending operation. The 1 indicates the buffer size of the channel which means it can only store one value.
+
+	go func() {
+		_, _, err := mg.Send(message)
+		resultChan <- err
+	}()
+
+	select {
+	case err := <-resultChan:
+		if err != nil {
+			return err
+		}
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+	return nil
+
 }
