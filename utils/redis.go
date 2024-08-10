3302ddd829557ebefcf5f470ba3dfc9ffcc49191
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-10 09:19:14 +0100

    feat:added goroutine to handle context

--- utils/redis.go
+++ utils/redis.go
@@ -1,14 +1,45 @@
 package utils
 
-import "github.com/go-redis/redis"
+import (
+	"context"
+	"log"
+	"time"
 
-//Intializes a Redis instance
+	"github.com/go-redis/redis"
+)
+
+// Intializes a Redis instance
 func InitRedisClient() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:3001", // Redis server address
+		Addr:     "localhost:3003", // Redis server address
 		Password: "",               // No password set
 		DB:       0,                // Use default DB
 	})
 
-	return rdb
+	// Creates a context with a timeout of 10 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	resultChan := make(chan error, 1)
+
+	go func() {
+		_, err := rdb.Ping().Result()
+		if err != nil {
+			resultChan <- err
+		} else {
+			close(resultChan)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		log.Fatalf("Context timeout: %v", ctx.Err())
+		return nil
+	case err := <-resultChan:
+		if err != nil {
+			log.Fatalf("Failed to connect to Redis: %v", err)
+			return nil
+		}
+		return rdb
+	}
 }
