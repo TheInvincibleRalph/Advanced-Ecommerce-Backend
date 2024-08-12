138c9a936e6885255511a2126f51213a72f2631a
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-12 14:46:26 +0100

    updated

--- handlers/admin.go
+++ handlers/admin.go
@@ -3,8 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/theinvincible/ecommerce-backend/models"
+	"github.com/theinvincible/ecommerce-backend/utils"
 )
 
 func AdminHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,4 +25,52 @@ func AdminMiddleware(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func AdminDashboardHandler(w http.ResponseWriter, r *http.Request) {
+	var dashboardData map[string]interface{}
+	cacheKey := "admin_dashboard"
+
+	// Attempt to get the cached dashboard data from Redis
+	cachedData, err := utils.InitRedisClient().Get(cacheKey).Result()
+	if err == redis.Nil {
+		// Cache miss: Fetch from the database
+		var userCount, productCount, orderCount int64
+
+		if err := db.Model(&models.User{}).Count(&userCount).Error; err != nil {
+			http.Error(w, "Failed to retrieve user count", http.StatusInternalServerError)
+			return
+		}
+
+		if err := db.Model(&models.Product{}).Count(&productCount).Error; err != nil {
+			http.Error(w, "Failed to retrieve product count", http.StatusInternalServerError)
+			return
+		}
+
+		if err := db.Model(&models.Order{}).Count(&orderCount).Error; err != nil {
+			http.Error(w, "Failed to retrieve order count", http.StatusInternalServerError)
+			return
+		}
+
+		dashboardData = map[string]interface{}{
+			"userCount":    userCount,
+			"productCount": productCount,
+			"orderCount":   orderCount,
+		}
+
+		// Cache the data in Redis with an expiration time
+		jsonData, _ := json.Marshal(dashboardData)
+		utils.InitRedisClient().Set(cacheKey, jsonData, time.Minute)
+	} else if err != nil {
+		// Redis error
+		http.Error(w, "Failed to retrieve data", http.StatusInternalServerError)
+		return
+	} else {
+		// Cache hit: Unmarshal the cached data
+		json.Unmarshal([]byte(cachedData), &dashboardData)
+	}
+
+	// Return the data as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(dashboardData)
 }
