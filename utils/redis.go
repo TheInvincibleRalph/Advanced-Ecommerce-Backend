afc7abd2ad5f716ed04ffabc0c21f625b2af53ee
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-09-13 13:53:17 +0100

    cleanup

--- utils/redis.go
+++ utils/redis.go
@@ -8,64 +8,6 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// Initializes a Redis instance
-// func InitRedisClient() *redis.Client {
-// 	rdb := redis.NewClient(&redis.Options{
-// 		Addr:     "localhost:6379", // Redis server address
-// 		Password: "",               // No password set
-// 		DB:       0,                // Use default DB
-// 	})
-
-// 	// Creates a context with a timeout of 10 seconds
-// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-// 	defer cancel()
-
-// 	resultChan := make(chan error, 1)
-
-// 	go func() {
-// 		_, err := rdb.Ping(ctx).Result() // Pass the context to Ping method
-// 		if err != nil {
-// 			resultChan <- err
-// 		} else {
-// 			close(resultChan)
-// 		}
-// 	}()
-
-// 	select {
-// 	case <-ctx.Done():
-// 		log.Fatalf("Context timeout: %v", ctx.Err())
-// 		return nil
-// 	case err := <-resultChan:
-// 		if err != nil {
-// 			log.Fatalf("Failed to connect to Redis: %v", err)
-// 			return nil
-// 		}
-// 		return rdb
-// 	}
-// }
-
-// func InitRedisClient() *redis.Client {
-// 	rdb := redis.NewClient(&redis.Options{
-// 		Addr:     "localhost:6379", // Redis server address
-// 		Password: "",               // No password set
-// 		DB:       0,                // Use default DB
-// 	})
-
-// 	// Context with a 5-second timeout
-// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-// 	defer cancel()
-
-// 	// Ping the Redis server
-// 	_, err := rdb.Ping(ctx).Result()
-// 	if err != nil {
-// 		log.Fatalf("Error connecting to Redis: %v", err)
-// 	} else {
-// 		fmt.Println("Connected to Redis successfully!")
-// 	}
-
-//		return rdb
-//	}
-
 var redisClient *redis.Client
 
 // InitRedisClient initializes the Redis client
